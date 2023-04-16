689a4bd236e30c61d5cc51fae29fc5980921c186
m-star18 <masutangoribura@gmail.com>
2023-04-16 22:11:31 +0900

    Add make_emb.py

---
+++ src/make_emb.py
@@ -0,0 +1,39 @@
+import os
+import argparse
+
+from langchain.document_loaders import OnlinePDFLoader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores.faiss import FAISS
+
+
+def main(args):
+    os.environ["OPENAI_API_KEY"] = args.OPEN_API_KEY
+
+    # 1. PDFを読み込み、Document objectを作ってください。
+    loader = OnlinePDFLoader(args.pdf_url)
+    pdf_docs = loader.load()
+
+    # 2. Document objectを読み込み適切なチャンクサイズに分割してください。
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=args.chunk_size,
+        chunk_overlap=args.chunk_overlap,
+    )
+
+    # 3. OpenAIEmbeddings と FAISS オブジェクトを作成してください。上で作ったチャンクをベクトル化し、保存してください。
+    documents = text_splitter.split_documents(pdf_docs)
+    embeddings = OpenAIEmbeddings()
+    faiss_db = FAISS.from_documents(documents, embeddings)
+    faiss_db.save_local(args.faiss_save_path)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--OPEN_API_KEY", type=str, default="")
+    parser.add_argument("--chunk_size", type=int, default=1000, help="Please specify the chunk_size for CharacterTextSplitter within a number less than or equal to 4096.")
+    parser.add_argument("--chunk_overlap", type=int, default=200, help="Please specify the chunk_overlap for CharacterTextSplitter within a number less than or equal to 4096.")
+    parser.add_argument("--faiss_save_path", type=str, default="faiss_pdf_index", help="Please specify the name of the created Faiss object.")
+    parser.add_argument("--pdf_url", type=str, default="", help="Please specify the path of the PDF file to be read.")
+    args = parser.parse_args()
+
+    main(args)
