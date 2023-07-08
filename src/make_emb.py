0b647c91814de92b04fc84e08c64c24bc14cc44b
m-star18 <masutangoribura@gmail.com>
2023-07-08 11:21:36 +0900

    Update make_emb.py (load pdf file)

--- src/make_emb.py
+++ src/make_emb.py
@@ -1,22 +1,22 @@
 import os
 import argparse
 
-from langchain.document_loaders import OnlinePDFLoader
+from langchain.document_loaders import OnlinePDFLoader, PyPDFLoader
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter, NLTKTextSplitter, CharacterTextSplitter
 from langchain.vectorstores.faiss import FAISS
 
 
 def main(args):
     os.environ["OPENAI_API_KEY"] = args.OPEN_API_KEY
-    
-    loader = OnlinePDFLoader(args.pdf_url)
+
+    if args.pdf_path.startswith("http"):
+        loader = OnlinePDFLoader(args.pdf_url)
+    elif args.pdf_path.endswith(".pdf"):
+        loader = PyPDFLoader(args.pdf_url)
+    else:
+        raise ValueError("Please specify the path of the PDF file to be read.")
     pdf_docs = loader.load()
-    
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=args.chunk_size,
-        chunk_overlap=args.chunk_overlap,
-    )
 
     # Create OpenAIEmbeddings and FAISS objects. Vectorize the chunks created above and save.
     documents = text_splitter.split_documents(pdf_docs)
@@ -30,8 +30,9 @@ if __name__ == "__main__":
     parser.add_argument("--OPEN_API_KEY", type=str, default="")
     parser.add_argument("--chunk_size", type=int, default=1000, help="Please specify the chunk_size for CharacterTextSplitter within a number less than or equal to 4096.")
     parser.add_argument("--chunk_overlap", type=int, default=200, help="Please specify the chunk_overlap for CharacterTextSplitter within a number less than or equal to 4096.")
-    parser.add_argument("--faiss_save_path", type=str, default="faiss_pdf_index", help="Please specify the name of the created Faiss object.")
-    parser.add_argument("--pdf_url", type=str, default="", help="Please specify the path of the PDF file to be read.")
+    parser.add_argument("--pdf_path", type=str, default="", help="Please specify the path of the pdf to be read.")
+    parser.add_argument("--split_mode", type=str, default="recursive_character", help="Please specify the split mode. (character, recursive_character, nltk, tiktoken)")
+    parser.add_argument("--faiss_save_path", type=str, default="faiss_index", help="Please specify the name of the created Faiss object.")
     args = parser.parse_args()
 
     main(args)
