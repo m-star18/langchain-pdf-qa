52af34af1a3f22fa5198225361595f1c5672b995
m-star18 <masutangoribura@gmail.com>
2023-07-08 11:23:34 +0900

    Update comments

--- src/make_emb.py
+++ src/make_emb.py
@@ -10,6 +10,7 @@ from langchain.vectorstores.faiss import FAISS
 def main(args):
     os.environ["OPENAI_API_KEY"] = args.OPEN_API_KEY
 
+    # Load the PDF file (if the file is a URL, load the PDF file from the URL)
     if args.pdf_path.startswith("http"):
         loader = OnlinePDFLoader(args.pdf_url)
     elif args.pdf_path.endswith(".pdf"):
@@ -17,6 +18,35 @@ def main(args):
     else:
         raise ValueError("Please specify the path of the PDF file to be read.")
     pdf_docs = loader.load()
+
+    # Split by separator and merge by character count
+    if args.split_mode == "character":
+        # Create a CharacterTextSplitter object
+        text_splitter = CharacterTextSplitter(
+            chunk_size=args.chunk_size,
+            chunk_overlap=args.chunk_overlap,
+        )
+    # Recursively split until below the chunk size limit
+    elif args.split_mode == "recursive_character":
+        # Create a RecursiveCharacterTextSplitter object
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=args.chunk_size,
+            chunk_overlap=args.chunk_overlap,
+        )
+    elif args.split_mode == "nltk":
+        # Create a NLTKTextSplitter object
+        text_splitter = NLTKTextSplitter(
+            chunk_size=args.chunk_size,
+            chunk_overlap=args.chunk_overlap,
+        )
+    elif args.split_mode == "tiktoken":
+        # Create a CharacterTextSplitter object
+        text_splitter = CharacterTextSplitter.from_tiktoken_encoder(
+            chunk_size=args.chunk_size,
+            chunk_overlap=args.chunk_overlap,
+        )
+    else:
+        raise ValueError("Please specify the split mode.")
 
     # Create OpenAIEmbeddings and FAISS objects. Vectorize the chunks created above and save.
     documents = text_splitter.split_documents(pdf_docs)
